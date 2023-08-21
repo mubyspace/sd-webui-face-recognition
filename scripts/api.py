ca815c956ecd40f42a270cd41b4af5db3154e412
zoumeibiao <zoumeibiao@qutoutiao.net>
2023-08-21 14:26:30 +0800

    change to use dlib lib

--- scripts/api.py
+++ scripts/api.py
@@ -1,50 +1,43 @@
 from fastapi import FastAPI, Body
-import cv2
-import insightface
 import numpy as np
+import cv2
 import base64
 from modules.api.models import *
-from modules.api import api
 import gradio as gr
-import os
+from io import BytesIO
+from PIL import Image
+import face_recognition as fr
 
-xml_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "haarcascade_frontalface_default.xml")
 
-def base64_to_cv2(base64_code):
-    img_data = base64.b64decode(base64_code)
-    img_array = np.fromstring(img_data, np.uint8)
-    img = cv2.imdecode(img_array, cv2.COLOR_RGB2BGR)
-    return img
+def base64_to_image(base64_str):
+    image = base64.b64decode(base64_str, altchars=None, validate=False)
+    image = BytesIO(image)
+    image = Image.open(image)
+    return image
+
 
 def face_recognition_api(_: gr.Blocks, app: FastAPI):
-	@app.post("/face-recongnition")
-	async def face_recognition(
-		input_image: str = Body("", title='face-recongnition input image')
-	):
-		img = base64_to_cv2(input_image)
+    @app.post("/face-recongnition")
+    async def face_recognition(
+            input_image: str = Body("", title='face-recongnition input image')
+    ):
+        image = base64_to_image(input_image)
+        img = np.array(image)
+        face_locations = fr.face_locations(img)
+        if len(face_locations) == 0:
+            return {"status": "Failed", "msg": "No real human face or no the full face in the input_image.", "data": ""}
+        elif len(face_locations) > 1:
+            return {"status": "Failed", "msg": "More than one face in the input_image.", "data": ""}
+        else:
+            top, right, bottom, left = face_locations[0]
+            face = img[top:bottom, left:right]
+            _, buffer = cv2.imencode('.png', face)
+            return {"status": "Successful", "msg": "Successful", "data": base64.b64encode(buffer).decode()}
 
-		model = insightface.app.FaceAnalysis(name="buffalo_l")
-		model.prepare(ctx_id=0, det_thresh=0.45, det_size=(640, 640))
-		insightface_faces = model.get(img)
-
-		face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		cv_faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-		if len(insightface_faces) == 0 and len(cv_faces) == 0:
-			return {"status": "Failed", "msg": "No faces in input_image"}
-		elif len(insightface_faces) > 0 and len(cv_faces) == 0:
-			return {"status": "Failed", "msg": "Please make sure that the input_image has the full face"}
-		elif len(insightface_faces) > 1 and len(cv_faces) > 0:
-			return {"status": "Failed", "msg": "There is more than one face in the input_image"}
-		else:
-			(x, y, w, h) = cv_faces[0]
-			face = img[y:y+h, x:x+w]
-			_, buffer = cv2.imencode('.png', face)
-			return {"status": "Successed", "image": base64.b64encode(buffer).decode()}
 
 try:
-	import modules.script_callbacks as script_callbacks
-	script_callbacks.on_app_started(face_recognition_api)
+    import modules.script_callbacks as script_callbacks
+
+    script_callbacks.on_app_started(face_recognition_api)
 except:
-	pass
+    pass
